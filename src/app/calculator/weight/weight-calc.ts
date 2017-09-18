0179db2be904798f93e3e86efe736ae1b6f75ce6
Zia Ur Rehman <ziaishere@gmail.com>
2017-09-18 13:15:36 +0530

    Update weight-calc.ts
    updating actual brass sheet weight

--- src/app/calculator/weight/weight-calc.ts
+++ src/app/calculator/weight/weight-calc.ts
@@ -20,27 +20,27 @@ export class WeightCalculator {
     rectangle.length = 48;
     rectangle.breadth = 24;
 
-    this.densitySheetMap.set(10, new Sheet(10, 11.7, rectangle));
-    this.densitySheetMap.set(11, new Sheet(11, 11.7, rectangle));
-    this.densitySheetMap.set(12, new Sheet(12, 11.7, rectangle));
-    this.densitySheetMap.set(13, new Sheet(13, 11.7, rectangle));
-    this.densitySheetMap.set(14, new Sheet(14, 11.7, rectangle));
-    this.densitySheetMap.set(15, new Sheet(15, 11.7, rectangle));
-    this.densitySheetMap.set(16, new Sheet(16, 11.7, rectangle));
-    this.densitySheetMap.set(17, new Sheet(17, 11.7, rectangle));
-    this.densitySheetMap.set(18, new Sheet(18, 11.7, rectangle));
-    this.densitySheetMap.set(19, new Sheet(19, 11.7, rectangle));
-    this.densitySheetMap.set(20, new Sheet(20, 11.7, rectangle));
-    this.densitySheetMap.set(21, new Sheet(21, 11.7, rectangle));
-    this.densitySheetMap.set(22, new Sheet(22, 11.7, rectangle));
-    this.densitySheetMap.set(23, new Sheet(23, 11.7, rectangle));
-    this.densitySheetMap.set(24, new Sheet(24, 11.7, rectangle));
-    this.densitySheetMap.set(25, new Sheet(25, 11.7, rectangle));
-    this.densitySheetMap.set(26, new Sheet(26, 11.7, rectangle));
-    this.densitySheetMap.set(27, new Sheet(27, 11.7, rectangle));
-    this.densitySheetMap.set(28, new Sheet(28, 11.7, rectangle));
-    this.densitySheetMap.set(29, new Sheet(29, 11.7, rectangle));
-    this.densitySheetMap.set(30, new Sheet(30, 11.7, rectangle));
+    this.densitySheetMap.set(10, new Sheet(10, 20.4, rectangle));
+    this.densitySheetMap.set(11, new Sheet(11, 18.6, rectangle));
+    this.densitySheetMap.set(12, new Sheet(12, 16.6, rectangle));
+    this.densitySheetMap.set(13, new Sheet(13, 14.7, rectangle));
+    this.densitySheetMap.set(14, new Sheet(14, 12.8, rectangle));
+    this.densitySheetMap.set(15, new Sheet(15, 11.5, rectangle));
+    this.densitySheetMap.set(16, new Sheet(16, 10.0, rectangle));
+    this.densitySheetMap.set(17, new Sheet(17, 9.0, rectangle));
+    this.densitySheetMap.set(18, new Sheet(18, 8.2, rectangle));
+    this.densitySheetMap.set(19, new Sheet(19, 6.4, rectangle));
+    this.densitySheetMap.set(20, new Sheet(20, 5.7, rectangle));
+    this.densitySheetMap.set(21, new Sheet(21, 5.1, rectangle));
+    this.densitySheetMap.set(22, new Sheet(22, 4.4, rectangle));
+    this.densitySheetMap.set(23, new Sheet(23, 3.8, rectangle));
+    this.densitySheetMap.set(24, new Sheet(24, 3.5, rectangle));
+    this.densitySheetMap.set(25, new Sheet(25, 3.2, rectangle));
+    this.densitySheetMap.set(26, new Sheet(26, 2.9, rectangle));
+    this.densitySheetMap.set(27, new Sheet(27, 2.6, rectangle));
+    this.densitySheetMap.set(28, new Sheet(28, 2.4, rectangle));
+    this.densitySheetMap.set(29, new Sheet(29, 2.2, rectangle));
+    this.densitySheetMap.set(30, new Sheet(30, 2.0, rectangle));
   }
 
   public calculateWeight(guage:number, shape:Shape):number {
